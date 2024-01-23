b7ba5c3ddcc600cf558a69d952b0a87dae69fa7d
MrDrovE <ilyxa.eletskiya@gmail.com>
2024-01-23 15:11:10 +0400

    Gamepad_support

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,36 +1,11 @@
 extends CharacterBody2D
 
 @export var speed = 400
-
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
-
+	
+	
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var direction = Input.get_axis("left", "right")
-	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
-
-	
-
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
