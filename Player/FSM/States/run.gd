8665e643ccba3a58805103e2c95401bf5db0ba2a
MrDrovE <ilyxa.eletskiya@gmail.com>
2024-01-31 13:47:23 +0400

    Fixes

--- Player/FSM/States/run.gd
+++ Player/FSM/States/run.gd
@@ -5,14 +5,15 @@ func enter(_msg:Dictionary={}):
 	
 func inner_physics_process(delta):
 	
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	var input_direction = Input.get_vector("left", "right", "up", "down") # Бег
 	var direction_x = Input.get_axis("left","right")
 	var direction_y = Input.get_axis("up","down")
 	var direction = Vector2(direction_x,direction_y).normalized()
 	player.velocity = input_direction * player.speed
-	player.move_and_slide()
-	print(name)	
+	player.move_and_slide() 
+	
 	if player.velocity == Vector2.ZERO:
 		state_machine.change_to("Idle")
-	
+		
 	player.animation.stop()
+	print(name)	
